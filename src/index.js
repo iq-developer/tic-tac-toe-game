f6b537cdaff74d09f4dace4fcb00c3f611938239
Vladimir Sevastianov <108.nimay@gmail.com>
2021-03-04 00:27:02 +0200

    feat: game process implement

--- src/index.js
+++ src/index.js
@@ -3,19 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class App extends React.Component {
+  constructor() {
+		super();
+		this.state = {finish: false, message: 'Play again'};
+	}
 
   play(event) {
+    let buttons = document.querySelectorAll('button');
+    let message = document.querySelector('#message');
 
-    if (event.target.innerHTML === '✖' || event.target.innerHTML === '⭘'){
+    if (this.state.finish) return;
+
+    if (event.target.innerHTML === '✖' || event.target.innerHTML === 'O'){
       alert('This place is already occupied. Try another one.');
       return;
     } else {
       event.target.innerHTML = '✖';
       event.target.classList.add('x');
+      message.className ='';
     }
-
-    let buttons = document.querySelectorAll('button');
-    let message = document.querySelector('#message');
 
     if (
         (buttons[0].innerHTML === '✖' && buttons[1].innerHTML === '✖' && buttons[2].innerHTML === '✖') ||
@@ -27,39 +33,43 @@ class App extends React.Component {
         (buttons[0].innerHTML === '✖' && buttons[4].innerHTML === '✖' && buttons[8].innerHTML === '✖') ||
         (buttons[2].innerHTML === '✖' && buttons[4].innerHTML === '✖' && buttons[6].innerHTML === '✖')
         ) {
-      message.classList.add('win');
-      message.innerHTML = '✖ wins. Play again?';
+      buttons.forEach(item => {
+        item.classList.add('disabled');
+      });    
+      message.className ='win';
+      this.setState({message: '✖ wins. Play again?'});
+      this.setState({finish: true});
       return;
     }
-
-    // Add ⭘ randomly
-    function nolls() {
-      let emptyCells = Array.from(buttons).filter(item => item.innerHTML === '&nbsp;');
+ 
+      let emptyCells = Array.from(buttons).filter(item => item.innerHTML === '');
       let randomCell = randomInteger(0, emptyCells.length-1);
       console.log({randomCell}, emptyCells.length-1);
 
       if (emptyCells[randomCell]){
-        emptyCells[randomCell].innerHTML = '⭘';
+        emptyCells[randomCell].innerHTML = 'O';
         emptyCells[randomCell].classList.add('o');
       }
 
       if (
-        (buttons[0].innerHTML === '⭘' && buttons[1].innerHTML === '⭘' && buttons[2].innerHTML === '⭘') ||
-        (buttons[3].innerHTML === '⭘' && buttons[4].innerHTML === '⭘' && buttons[5].innerHTML === '⭘') ||
-        (buttons[6].innerHTML === '⭘' && buttons[7].innerHTML === '⭘' && buttons[8].innerHTML === '⭘') ||
-        (buttons[0].innerHTML === '⭘' && buttons[3].innerHTML === '⭘' && buttons[6].innerHTML === '⭘') ||
-        (buttons[1].innerHTML === '⭘' && buttons[4].innerHTML === '⭘' && buttons[7].innerHTML === '⭘') ||
-        (buttons[2].innerHTML === '⭘' && buttons[5].innerHTML === '⭘' && buttons[8].innerHTML === '⭘') ||
-        (buttons[0].innerHTML === '⭘' && buttons[4].innerHTML === '⭘' && buttons[8].innerHTML === '⭘') ||
-        (buttons[2].innerHTML === '⭘' && buttons[4].innerHTML === '⭘' && buttons[6].innerHTML === '⭘')
+        (buttons[0].innerHTML === 'O' && buttons[1].innerHTML === 'O' && buttons[2].innerHTML === 'O') ||
+        (buttons[3].innerHTML === 'O' && buttons[4].innerHTML === 'O' && buttons[5].innerHTML === 'O') ||
+        (buttons[6].innerHTML === 'O' && buttons[7].innerHTML === 'O' && buttons[8].innerHTML === 'O') ||
+        (buttons[0].innerHTML === 'O' && buttons[3].innerHTML === 'O' && buttons[6].innerHTML === 'O') ||
+        (buttons[1].innerHTML === 'O' && buttons[4].innerHTML === 'O' && buttons[7].innerHTML === 'O') ||
+        (buttons[2].innerHTML === 'O' && buttons[5].innerHTML === 'O' && buttons[8].innerHTML === 'O') ||
+        (buttons[0].innerHTML === 'O' && buttons[4].innerHTML === 'O' && buttons[8].innerHTML === 'O') ||
+        (buttons[2].innerHTML === 'O' && buttons[4].innerHTML === 'O' && buttons[6].innerHTML === 'O')
         ) {
-      message.classList.add('loose');
-      message.innerHTML = '⭘ wins. Play again?';
-      return;
-    }
-    }
-
-    nolls();
+        buttons.forEach(item => {
+          item.classList.add('disabled');
+        });  
+        message.className ='loose';
+        this.setState({message: 'O wins. Play again?'});
+        this.setState({finish: true});
+        return;
+      }
+    
 
     function randomInteger(min, max) {
       let rand = min - 0.5 + Math.random() * (max - min + 1);
@@ -69,41 +79,72 @@ class App extends React.Component {
   }
 
   playAgain() {
-    alert('Not implement :) Please reload page if you wish to play again');
+    let buttons = document.querySelectorAll('button');
+    buttons.forEach(item => {
+      item.innerHTML = '';
+      item.className = 'bar';
+    });
+    let message = document.querySelector('#message');
+    message.className = 'disabled';
+    this.setState({message: 'Play again'});
+    this.setState({finish: false});
   }
+
+  fullscreen(elem) {
+    elem = elem || document.documentElement;
+    if (!document.fullscreenElement && !document.mozFullScreenElement &&
+      !document.webkitFullscreenElement && !document.msFullscreenElement) {
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+      } else if (elem.mozRequestFullScreen) {
+        elem.mozRequestFullScreen();
+      } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+      }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      }
+    }
+    
+  }
+  
 
   render() {
     return (
     <div className="wrapper">
       <div className="header">
-        <h1>Tic-tac-toe game</h1>
+        <h1>Tic-tac-toe game</h1> 
       </div>
-      <div id="message" className="normal" onClick={this.playAgain.bind(this)}>Play again</div>
-      <div className="field">
-        <button className="bar" onClick={this.play.bind(this)}>&nbsp;</button>
-        <button className="bar" onClick={this.play.bind(this)}>&nbsp;</button>
-        <button className="bar" onClick={this.play.bind(this)}>&nbsp;</button>
-        <button className="bar" onClick={this.play.bind(this)}>&nbsp;</button>
-        <button className="bar" onClick={this.play.bind(this)}>&nbsp;</button>
-        <button className="bar" onClick={this.play.bind(this)}>&nbsp;</button>
-        <button className="bar" onClick={this.play.bind(this)}>&nbsp;</button>
-        <button className="bar" onClick={this.play.bind(this)}>&nbsp;</button>
-        <button className="bar" onClick={this.play.bind(this)}>&nbsp;</button>
+      <div id="btnfullscreen" onClick={this.fullscreen.bind(this, document.documentElement)}>Fullscreen on/off</div>
+      <div id="message" className="disabled" onClick={this.playAgain.bind(this)}>{this.state.message}</div>
+         <div className="field">
+        <button className="bar" onClick={this.play.bind(this)}></button>
+        <button className="bar" onClick={this.play.bind(this)}></button>
+        <button className="bar" onClick={this.play.bind(this)}></button>
+        <button className="bar" onClick={this.play.bind(this)}></button>
+        <button className="bar" onClick={this.play.bind(this)}></button>
+        <button className="bar" onClick={this.play.bind(this)}></button>
+        <button className="bar" onClick={this.play.bind(this)}></button>
+        <button className="bar" onClick={this.play.bind(this)}></button>
+        <button className="bar" onClick={this.play.bind(this)}></button>
       </div>
       <div className="footer">
+
       </div>
     </div>
     );
   }
 }
 
-// class Bar extends React.Component {
-//   render() {
-//     return (
-//       <button className="bar" onClick={this.play.bind(this)}></button>
-//     );
-//   }
-// }
 
 
 
