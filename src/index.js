269c7036e50d76382cab282b9a7bd65ec0fa8096
Vladimir Sevastianov <108.nimay@gmail.com>
2021-03-03 18:11:35 +0200

    feat: add basic styles and functions

--- src/index.js
+++ src/index.js
@@ -3,33 +3,51 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class App extends React.Component {
+
+  play(event) {
+    console.log(event.target);
+    if (event.target.innerHTML === '✖' || event.target.innerHTML === '⭘'){
+      alert('это место занято :)');
+    } else {
+      event.target.innerHTML = '✖';
+      event.target.classList.add('x');
+    }
+
+
+
+  }
+
   render() {
     return (
-    <div>
-    <div className="header">
-      <h1>Tic-tac-toe game</h1>
-    </div>
-    <div className="field">
-      <Bar /><Bar /><Bar />
-      <Bar /><Bar /><Bar />
-      <Bar /><Bar /><Bar />
-    </div>
-    <div className="footer">
-    </div>
+    <div className="wrapper">
+      <div className="header">
+        <h1>Tic-tac-toe game</h1>
+      </div>
+      <div className="field">
+        <button className="bar" onClick={this.play.bind(this)}></button>
+        <button className="bar" onClick={this.play.bind(this)}></button>
+        <button className="bar" onClick={this.play.bind(this)}></button>
+        <button className="bar" onClick={this.play.bind(this)}></button>
+        <button className="bar" onClick={this.play.bind(this)}></button>
+        <button className="bar" onClick={this.play.bind(this)}></button>
+        <button className="bar" onClick={this.play.bind(this)}></button>
+        <button className="bar" onClick={this.play.bind(this)}></button>
+        <button className="bar" onClick={this.play.bind(this)}></button>
+      </div>
+      <div className="footer">
+      </div>
     </div>
     );
   }
 }
 
-class Bar extends React.Component {
-  render() {
-    return (
-      <button className="bar">
-        
-      </button>
-    );
-  }
-}
+// class Bar extends React.Component {
+//   render() {
+//     return (
+//       <button className="bar" onClick={this.play.bind(this)}></button>
+//     );
+//   }
+// }
 
 
 
