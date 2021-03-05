0f31a2c3f683c4d3e3b4d1221937732fa0a7cebd
Vladimir Sevastianov <108.nimay@gmail.com>
2021-03-05 18:43:58 +0200

    feat: react-hooks implement

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class App extends React.Component {
-  constructor() {
-		super();
-		this.state = {finish: false, message: 'Play again'};
-	}
+function App1() {
+  const [message, setMessage] = useState('Play again');
+  const [isFinished, setIsFinished] = useState(false);
 
-  play(event) {
+  return (
+    <div className="wrapper">
+      <div className="header">
+        <h1>Tic-tac-toe React game</h1>
+      </div>
+      <div id="btnfullscreen" onClick={fullscreen}>Fullscreen on/off</div>
+      <div id="message" className="disabled" onClick={playAgain}>{message}</div>
+        <div className="field">
+        <button className="bar" onClick={play}></button>
+        <button className="bar" onClick={play}></button>
+        <button className="bar" onClick={play}></button>
+        <button className="bar" onClick={play}></button>
+        <button className="bar" onClick={play}></button>
+        <button className="bar" onClick={play}></button>
+        <button className="bar" onClick={play}></button>
+        <button className="bar" onClick={play}></button>
+        <button className="bar" onClick={play}></button>
+      </div>
+      <Footer />
+
+    </div>
+  );
+
+  function play(event) {
     let buttons = document.querySelectorAll('button');
     let message = document.querySelector('#message');
 
-    if (this.state.finish) return;
+    if (isFinished) return;
 
     if (event.target.innerHTML === '✖' || event.target.innerHTML === 'O'){
       alert('This place is already occupied. Try another one.');
@@ -35,13 +57,13 @@ class App extends React.Component {
         ) {
       buttons.forEach(item => {
         item.classList.add('disabled');
-      });    
+      });
       message.className ='win';
-      this.setState({message: '✖ wins. Play again?'});
-      this.setState({finish: true});
+      setMessage('✖ wins. Play again?');
+      setIsFinished(true);
       return;
     }
- 
+
       let emptyCells = Array.from(buttons).filter(item => item.innerHTML === '');
       let randomCell = randomInteger(0, emptyCells.length-1);
       console.log({randomCell}, emptyCells.length-1);
@@ -63,13 +85,12 @@ class App extends React.Component {
         ) {
         buttons.forEach(item => {
           item.classList.add('disabled');
-        });  
+        });
         message.className ='loose';
-        this.setState({message: 'O wins. Play again?'});
-        this.setState({finish: true});
+        setMessage('O wins. Play again?');
+        setIsFinished(true);
         return;
       }
-    
 
     function randomInteger(min, max) {
       let rand = min - 0.5 + Math.random() * (max - min + 1);
@@ -78,78 +99,22 @@ class App extends React.Component {
 
   }
 
-  playAgain() {
-    let buttons = document.querySelectorAll('button');
-    buttons.forEach(item => {
-      item.innerHTML = '';
-      item.className = 'bar';
-    });
-    let message = document.querySelector('#message');
-    message.className = 'disabled';
-    this.setState({message: 'Play again'});
-    this.setState({finish: false});
+  function playAgain() {
   }
 
-  fullscreen(elem) {
-    elem = elem || document.documentElement;
-    if (!document.fullscreenElement && !document.mozFullScreenElement &&
-      !document.webkitFullscreenElement && !document.msFullscreenElement) {
-      if (elem.requestFullscreen) {
-        elem.requestFullscreen();
-      } else if (elem.msRequestFullscreen) {
-        elem.msRequestFullscreen();
-      } else if (elem.mozRequestFullScreen) {
-        elem.mozRequestFullScreen();
-      } else if (elem.webkitRequestFullscreen) {
-        elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-      }
-    } else {
-      if (document.exitFullscreen) {
-        document.exitFullscreen();
-      } else if (document.msExitFullscreen) {
-        document.msExitFullscreen();
-      } else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-      } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-      }
-    }
-    
-  }
-  
-
-  render() {
-    return (
-    <div className="wrapper">
-      <div className="header">
-        <h1>Tic-tac-toe React game</h1> 
-      </div>
-      <div id="btnfullscreen" onClick={this.fullscreen.bind(this, document.documentElement)}>Fullscreen on/off</div>
-      <div id="message" className="disabled" onClick={this.playAgain.bind(this)}>{this.state.message}</div>
-        <div className="field">
-        <button className="bar" onClick={this.play.bind(this)}></button>
-        <button className="bar" onClick={this.play.bind(this)}></button>
-        <button className="bar" onClick={this.play.bind(this)}></button>
-        <button className="bar" onClick={this.play.bind(this)}></button>
-        <button className="bar" onClick={this.play.bind(this)}></button>
-        <button className="bar" onClick={this.play.bind(this)}></button>
-        <button className="bar" onClick={this.play.bind(this)}></button>
-        <button className="bar" onClick={this.play.bind(this)}></button>
-        <button className="bar" onClick={this.play.bind(this)}></button>
-      </div>
-      <div className="footer">
-      
-      This game developed in 2021 by <a href="https://github.com/iq-developer">Vladimir Sevastianov</a> at <a href="https://rs.school/js/"><img alt="RS School logo" src="https://rs.school/images/rs_school_js.svg" width="100" /></a>  
-      </div>
-    </div>
-    );
+  function fullscreen() {
   }
 }
 
-
-
+function Footer() {
+  return (
+    <div className="footer">
+      This game developed in 2021 by <a href="https://github.com/iq-developer">Vladimir Sevastianov</a> at <a href="https://rs.school/js/"><img alt="RS School logo" src="https://rs.school/images/rs_school_js.svg" width="100" /></a>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <App />,
+  <App1 />,
   document.getElementById('root')
 );
