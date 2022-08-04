15f6ff7b44fc9b19dbd9cc81de965cdda0f47b89
Volodymyr Sevastianov <volodymyr@nodon.se>
2022-08-04 18:43:36 +0300

    fix: fullscreen

--- src/helpers/fullscreen.tsx
+++ src/helpers/fullscreen.tsx
@@ -1,29 +1,8 @@
-const Element_Copy=Element;                         // Magic
-function fullscreen(): void {
-    const document:any = window.document;           // Magic
-    const Element:any = Element_Copy;               // Magic
-    const fs = Element[0];
-    if (!document.fullscreenElement &&    // alternative standard method
-        !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {  // current working methods
-        if (fs.requestFullscreen) {
-            fs.requestFullscreen();
-        } else if (fs.msRequestFullscreen) {
-            fs.msRequestFullscreen();
-        } else if (fs.mozRequestFullScreen) {
-            fs.mozRequestFullScreen();
-        } else if (fs.webkitRequestFullscreen) {
-            fs.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-        }
-    } else {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.msExitFullscreen) {
-            document.msExitFullscreen();
-        } else if (document.mozCancelFullScreen) {
-            document.mozCancelFullScreen();
-        } else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen();
-        }
-    }
+function fullscreen() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else if (document.exitFullscreen) {
+    document.exitFullscreen();
+  }
 }
 export default fullscreen;
